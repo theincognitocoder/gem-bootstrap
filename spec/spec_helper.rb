6050d38791faa763dd65bad595998bd29f506acf
The Incognito Coder <theincognitocoder@gmail.com>
2018-06-08 10:25:07 -0700

    Improve rubocop config to allow for ideal formatting of multiline lists as arguments.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,10 @@ $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-  [
-    Coveralls::SimpleCov::Formatter,
-    SimpleCov::Formatter::HTMLFormatter,
-  ]
-)
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  Coveralls::SimpleCov::Formatter,
+  SimpleCov::Formatter::HTMLFormatter,
+])
 SimpleCov.start
 
 require 'gem-bootstrap'
